176e6acc3a4a20ce5b347e090df268d85307c172
guciano <adamothasha@gmail.com>
2023-06-05 21:11:32 +0700

    completing phonebook 2.8: adding phone number form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,27 +2,50 @@ import { useState } from 'react'
 //import Number from './components/Number'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
     const lowerCaseName = newName.toLowerCase();
-    if (persons.map((person) => person.name.toLowerCase() === lowerCaseName).length > 0) {
-      alert(`${newName} is already added to the phonebook.`)
+    const isNameExists = persons.some(
+      (person) =>
+        person.name.toLowerCase() === lowerCaseName || person.number === newNumber
+    );
+    const isNameValid = newName.trim().length > 0;
+    
+    if (isNameExists) {
+      alert(`${newName} is already added to the phonebook`)
       return
     }
 
-    const newPerson = { name: newName }
-    setPersons([...persons, newPerson])
+    if (!isNameValid) {
+      alert('Please enter a valid name.')
+      return
+    }
+
+    const newPerson = { 
+      name: newName,
+      number: newNumber,
+    }
+    const updatedPersons = persons.filter(
+      (person) =>
+        person.name.toLowerCase() !== lowerCaseName || person.number !== newNumber
+    );
+
+    setPersons([...updatedPersons, newPerson])
     setNewName(' ')
+    setNewNumber(' ')
   }
 
   return (
@@ -33,13 +56,16 @@ function App() {
           name: <input value={newName} onChange={handleName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, i) => (
-          <li key={i}>{person.name}</li>
+          <li key={i}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
