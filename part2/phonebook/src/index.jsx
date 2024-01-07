91fd641dd73d71cebbeee88d9bc24c344f19f413
guciano <adamothasha@gmail.com>
2024-01-08 03:06:51 +0700

    Getting data from server

--- part2/phonebook/src/index.jsx
+++ part2/phonebook/src/index.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom/client"
-
+import axios from 'axios'
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
 
+axios.get('http://localhost:3001/persons').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />);
+  });
