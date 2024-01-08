7e74bda0ebe6005339595ef11d51c87c2965daa1
guciano <adamothasha@gmail.com>
2024-01-08 22:08:48 +0700

    completing part 2.11

--- part2/phonebook/src/index.jsx
+++ part2/phonebook/src/index.jsx
@@ -1,10 +1,4 @@
-import ReactDOM from "react-dom/client"
-import axios from 'axios'
-import App from "./App"
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-axios.get('http://localhost:3001/persons').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />);
-  });
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
